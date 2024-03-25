95d9356da03f4015038a82eb54852deab916a8e4
Subir Das <dasjoy104@gamil.com>
2024-03-25 13:06:01 +0600

    all added

--- components/SportsCard.tsx
+++ components/SportsCard.tsx
@@ -20,15 +20,19 @@ const SportsCard = ({ sport }: SportCardProps) => {
         year,
     } = sport;
   return (
-    <div className='sport-card group'>
-        <div>
-            <h2>
-                {city_mpg}{combination_mpg} 
-            </h2>
-            <p>
-                {fuel_type}
-            </p>
-        </div>
+    <div >
+      
+
+        <div className="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
+	<img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-3xl font-semibold tracking-wide">{city_mpg}</h2>
+			<p className="dark:text-gray-800">{combination_mpg} </p>
+		</div>
+		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">Read more</button>
+	</div>
+</div>
     </div>
   );
 };
